738a945f708b31369aa3f3f3098444601ff3f5f5
amay22 <amay@nyu.edu>
2016-03-20 21:13:54 -0400

    Added User Service

---
+++ src/app/services/user.service.js
@@ -0,0 +1,36 @@
+(function() {
+
+  angular
+    .module('nike')
+    .service('userService', userService);
+
+  userService.$inject = ['$http', '$q', 'CONFIG'];
+
+  function userService($http, $q, CONFIG) {
+    var userServe = this;
+
+    userServe.login = function(user){
+        return $http.post(CONFIG.API_END_POINT + '/users/login', user)
+            .then(successFn, errorFn);
+    }
+
+    userServe.register = function(user){
+        return $http.post(CONFIG.API_END_POINT + '/users/new', user)
+            .then(successFn, errorFn);
+    }
+
+    userServe.getUserByEmail = function(email){
+        var dot = email.lastIndexOf('.');
+        return $http.get(CONFIG.API_END_POINT + '/users/email/' + email.substring(0,dot) + '/' + email.substring(dot+1))
+            .then(successFn, errorFn);
+    }
+
+    function successFn (response) {
+      return response.data;
+    }
+
+    function errorFn (response) {
+      return $q.reject(response.status);
+    }
+  }
+})();
